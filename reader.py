19ae5fde8a8eac34e933418915ea266e351a0932
nmishra <nmishra@yamabuki.isglobal.lan>
2023-07-05 16:03:27 +0200

    try reader

---
+++ reader.py
@@ -0,0 +1,81 @@
+"""
+Tweet Reader
+"""
+
+import json
+import os
+import codecs
+import logging
+import pickle
+
+from nltk.corpus.reader.api import CorpusReader
+from nltk.corpus.reader.api import CategorizedCorpusReader
+
+from nltk import pos_tag, sent_tokenize, wordpunct_tokenize
+
+log = logging.getLogger("log")
+log.setLevel('WARNING')
+
+CAT_PATTERN = r'([a-z_\s]+)/.*'
+DOC_PATTERN = r'(?!\.)[a-z_\s]+/[a-f0-9]+\.json'
+
+class JSONCorpusReader(CategorizedCorpusReader, CorpusReader):
+    """
+    A corpus reader for raw line-delimited JSON documents to enable preprocessing.
+    """
+
+    def __init__(self, root, fileids=DOC_PATTERN, encoding='utf8', **kwargs):
+        """
+        Initialize the corpus reader.  Categorization arguments
+        (``cat_pattern``, ``cat_map``, and ``cat_file``) are passed to
+        the ``CategorizedCorpusReader`` constructor.  The remaining
+        arguments are passed to the ``CorpusReader`` constructor.
+        """
+        # Add the default category pattern if not passed into the class.
+        if not any(k.startswith('cat_') for k,v in kwargs.items()):
+            kwargs['cat_pattern'] = CAT_PATTERN
+
+        # Initialize the NLTK corpus reader objects
+        CategorizedCorpusReader.__init__(self, kwargs)
+        CorpusReader.__init__(self, root, fileids, encoding)
+
+    def resolve(self, fileids, categories):
+        """
+        Returns a list of fileids or categories depending on what is passed
+        to each internal corpus reader function. Implemented similarly to
+        the NLTK ``CategorizedPlaintextCorpusReader``.
+        """
+        if fileids is not None and categories is not None:
+            raise ValueError("Specify fileids or categories, not both")
+
+        if categories is not None:
+            return self.fileids(categories)
+        return fileids
+
+    def docs(self, fileids=None, categories=None):
+        """
+        Returns the complete tweets of a JSON document, closing the document
+        after we are done reading it and yielding it in a memory safe fashion.
+        """
+        # Resolve the fileids and the categories
+        fileids = self.resolve(fileids, categories)
+
+        # Create a generator, loading one document into memory at a time.
+        for path, encoding in self.abspaths(fileids, include_encoding=True):
+            with codecs.open(path, 'r', encoding=encoding) as f:
+                yield f.read()
+
+    def sizes(self, fileids=None, categories=None):
+        """
+        Returns a list of tuples, the fileid and size on disk of the file.
+        This function is used to detect oddly large files in the corpus.
+        """
+        # Resolve the fileids and the categories
+        fileids = self.resolve(fileids, categories)
+
+        # Create a generator, getting every path and computing filesize
+        for path in self.abspaths(fileids):
+            yield os.path.getsize(path)
+
+if __name__ == '__main__':
+    test = JSONCorpusReader("/PROJECTES/ADAPTATION/proj/nmishra/p20230515_NM_tweets_learning/tweets/data")
